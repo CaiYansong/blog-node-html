65845b966d7241573e953c2d419f026771809c41
YansongCai <821566770@qq.com>
2019-12-10 15:37:51 +0800

    Create add-article.js

---
+++ server/add-article.js
@@ -0,0 +1,50 @@
+var fs = require('fs');
+
+var getHtml = require('./page-tpl');
+var getArticleTpl = require('./article-tpl');
+
+
+/**
+ * 添加文章
+ * @param {*} articlePath 
+ * @param {*} data 
+ * @param {*} cb 
+ * 
+{
+  id: 00001,
+  title: '',
+  content: '',
+  type: '',
+  dateTime: '',
+  ip: ''
+}
+ */
+function addArticle(articlePath, data, cb = () => { }) {
+    if (typeof data !== 'object') {
+        cb('{success: false}');
+        return;
+    }
+    if (!(data.title && !data.title.indexOf('0418'))) {
+        cb('{success: false, msg: "没有权限"}');
+        return;
+    }
+    data.title = data.title && data.title.slice(4);
+
+    var path = articlePath;
+    if (data.type) {
+        path += '/' + data.type;
+    }
+    path += '/' + data.title + '-' + data.dateTime + '.html';
+
+    var articleTpl = getArticleTpl(data);
+    var html = getHtml(articleTpl);
+    fs.writeFile(path, html, function (err) {
+        if (err) {
+            cb(err);
+        } else {
+            cb();
+        }
+    });
+}
+
+module.exports = addArticle;
